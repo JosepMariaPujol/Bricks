f391cb5fb65fcaeb7a5c84a35aaa59a247839c65
JosepMariaPujol <josepmpujol1@gmail.com>
2025-11-10 10:03:11 +0100

    Fixing About window

--- BricksProject/Assets/UI Toolkit/B02_AboutMenu/AboutMenuEvents.cs
+++ BricksProject/Assets/UI Toolkit/B02_AboutMenu/AboutMenuEvents.cs
@@ -6,7 +6,18 @@ public class AboutMenuEvents : MonoBehaviour
 {
     private UIDocument uiDocument;
     private Button backButton;
+    private TextField aboutInput;
+    private string aboutString = 
+        "BricksProject is a Unity-based tool designed to import, parse, and visualize LXFML data. \n" +
+        "It reads Digital Designer model files (.LXFML), extracts information about bricks, parts, \n" +
+        "and assemblies, and presents them interactively using the Unity UI Toolkit interface. \n\n" +
+        "This project demonstrates a workflow for interpreting structured XML data formats in Unity, \n" +
+        "as part of an ongoing exploration into procedural content, custom import pipelines, \n " +
+        "and UI-driven data visualization. \n\n" +
+        "Developed and maintained by Josep Maria Pujol.";
 
+    
+    
     public Transform mainMenuCameraTransform;
 
     private void Awake()
@@ -15,7 +26,10 @@ public class AboutMenuEvents : MonoBehaviour
         var root = uiDocument.rootVisualElement;
 
         // Query UI elements by name
+        aboutInput = root.Q<TextField>("AboutInput");
         backButton = root.Q<Button>("BackButton");
+        
+        aboutInput.value = aboutString;
         
         // Register button callback
         backButton?.RegisterCallback<ClickEvent>(OnBackClick);
